3817ab448275b663e2baa0031728059ecada8afc
root <root@Unbound.localdomain>
2019-09-13 22:55:53 -0600

    fixed style errors(pep8)

--- style.py
+++ style.py
@@ -1,75 +1,103 @@
-import sys, os
+import sys
+import os
+
 
 def one(): print('one')
+
+
 def two(): print('two')
+
+
 def three(): print('three')
+
+
 def four(): print('four')
-def five(a,b,c,d,e): print(a,b,c,d,e)
+
+
+def five(a, b, c, d, e): print(a, b, c, d, e)
+
+
 def throw(): raise Exception('threw')
+
+
 def oops(): print('oops')
 
-def FindSum(var_one, var_two, var_three,
-  var_four):
-    s = (var_one + 
-            var_two + 
-            var_three + 
-            var_four)
-    return s 
 
-def complex(real, imag = 0.0):
-    return magic(r = real, i = imag)
+def FindSum(var_one, var_two, var_three, var_four):
+    s = (var_one + var_two + var_three + var_four)
+    return s
 
-I=1
-O=1
-k=1
-l=1
 
-s = FindSum(I, O,
-    k, l)
+def complex(real, imag=0.0):
+    return magic(r=real, i=imag)
 
-print ( s )
 
-A = [1,2,3,4,5]
+var_I = 1
+var_O = 1
+var_k = 1
+var_l = 1
 
-print( A[ 1 ], { 'foo': 2 } )
+
+s = FindSum(var_I, var_O,
+            var_k, var_l)
+
+print(s)
+
+A = [1, 2, 3, 4, 5]
+
+print(A[1], {'foo': 2})
 
 x = 1
 y = 2
 
-if x == 4 : print (x , y ); x , y = y , x
+if x == 4:
+    print(x, y)
+    x, y = y, x
 
 dct = {}
-lst = [1,2,3,4]
-index=3
-dct ['key'] = lst [index]
+lst = [1, 2, 3, 4]
+index = 3
+dct['key'] = lst[index]
 
-x             = 1
-y             = 2
+x = 1
+y = 2
 long_variable = 3
 
-i=0
-i=i+1
+i = 0
+i = i + 1
 
 submitted = 0
-submitted +=1
+submitted += 1
 
 x = x * 2 - 1
 hypot2 = x * x + y * y
 x = (x + y) * (x - y)
 
 foo = 'blah'
-if foo == 'blah': one()
-two(); three(); four()
+if foo == 'blah':
+    one()
+    two()
+    three()
+    four()
 
 foo = 'non_blah'
-if foo == 'blah': four()
-else: one()
+if foo == 'blah':
+    four()
+else:
+    one()
 
-try: throw()
-except: oops()
-finally: one()
+try:
+    throw()
+except Exception:
+    oops()
+finally:
+    one()
 
-one(); two(); five(I, O, k,
-                   l, x)
+one()
+two()
+five(I, O, k, l, x)
 
-if foo == 'blah': one(); two(); three()
+if foo == 'blah':
+    one()
+    two()
+    three()
