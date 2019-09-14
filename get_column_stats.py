523bd2ff951a877982646d7c0c47eb83b42c92d3
root <root@Unbound.localdomain>
2019-09-13 23:47:08 -0600

    added catches, main, function defs

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,19 +1,73 @@
-import sys, math
+import sys
+import math
+import argparse
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
 
-f = open(file_name, 'r')
+def calculate_stdev(vec):
+    try:
+        stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+        return stdev
+    except Exception:
+        print("calculation of standard deviation failed")
 
-V = []
 
-for l in f:
-    A = [int(x) for x in l.split()]
-    V.append(A[col_num])
+def calculate_mean(vec):
+    try:
+        mean = sum(V) / len(V)
+        return mean
+    except Exception:
+        "error in dividing sum of column by length of column"
 
-mean = sum(V)/len(V)
 
-stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                description="Open a file and get mean, stddev of column")
 
-print('mean:', mean)
-print('stdev:', stdev)
+    parser.add_argument('--input_filename',
+                        type=str,
+                        help='name of input file',
+                        required=True)
+
+    parser.add_argument('--column_number',
+                        type=int,
+                        help='column number to use',
+                        required=True)
+
+    args = parser.parse_args()
+
+    file_name = args[0]
+    col_num = args[1]
+
+    try:
+        file_f = open(file_name, 'r')
+    except FileNotFoundError:
+        print('file not found')
+
+    vec_V = []
+
+    try:
+        for l in file_f:
+            A = [int(x) for x in l.split()]
+            var_V.append(A[col_num])
+    except Exception:
+        print("error in trying to parse line to retrieve column")
+
+    try:
+        mean = calculate_mean(vec_V)
+    except Exception:
+        print("error in calling calculate_mean function")
+
+    try:
+        stdev = calculate_stdev(vec_V)
+    except Exception:
+        print("error in calling calculate_stdev function")
+
+    try:
+        print('mean:', mean)
+    except Exception:
+        print("failed to print mean")
+
+    try:
+        print('stdev:', stdev)
+    except Exception:
+        print("failed to print stdev")
